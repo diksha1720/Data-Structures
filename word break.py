01516f03b256427243e61a6014b9f259c47b99d0
diksha choudhary <43616845+diksha1720@users.noreply.github.com>
2021-12-21 20:57:04 +0530

    Update word break.py

--- word break.py
+++ word break.py
@@ -5,9 +5,13 @@
  
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+   
+   
         n=len(s)
         dp = [False for _ in range(n+1)]
         dp[0]=True
+    
+    
     
         for i in range(n):
             if dp[i]:
